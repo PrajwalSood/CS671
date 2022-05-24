816621b55239d8ec5e4c28c959897d1ee1b83cee
Prajwal Sood <prajwalsood@gmail.com>
2022-05-24 21:31:34 +0530

    Added q1 2 layer metrics

---
+++ Assignment 4/q1_rnn2.py
@@ -0,0 +1,145 @@
+import os
+from pprint import pprint
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from data_loader import load_hw
+from tensorflow import keras
+import time
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_hw()
+# X_train = X_train.astype(np.float32)
+# y_train = y_train.astype(np.float32)
+
+# train = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+
+
+model = tf.keras.models.Sequential()
+
+model.add(tf.keras.layers.SimpleRNN(64, return_sequences=True, input_shape=(None, 2), activation = 'tanh'))
+model.add(tf.keras.layers.SimpleRNN(64, return_sequences=False, activation = 'tanh'))
+model.add(tf.keras.layers.Dense(32, activation='sigmoid'))
+model.add(tf.keras.layers.Dense(5, activation='softmax'))
+
+model.summary()
+
+
+optimizer = keras.optimizers.Adam(learning_rate=1e-3)
+
+loss_fn = keras.losses.CategoricalCrossentropy(from_logits = True)
+train_acc_metric = keras.metrics.CategoricalAccuracy()
+
+loss = []
+acc = []
+
+vloss = []
+vacc = []
+
+@tf.function(experimental_relax_shapes=True)
+def train_step(x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x, training=True)
+        loss_value = loss_fn(y, logits)
+    grads = tape.gradient(loss_value, model.trainable_weights)
+    optimizer.apply_gradients(zip(grads, model.trainable_weights))
+    train_acc_metric.update_state(y, logits)
+    return loss_value
+
+@tf.function(experimental_relax_shapes=True)
+def val_step(x, y):
+    with tf.GradientTape() as tape:
+        logits = model(x, training=True)
+        loss_value = loss_fn(y, logits)
+    return loss_value
+
+epochs = 400
+prev_loss = 1e10
+for epoch in range(epochs):
+  print("\nStart of epoch %d" % (epoch,))
+  start_time = time.time()
+  prev_loss = 1e10
+  for step,i in enumerate(range(len(X_train))):
+    loss_value = train_step(X_train[i].astype(np.float32), y_train[i].astype(np.float32))
+    val_loss = val_step(X_train[i//5].astype(np.float32), y_train[i//5].astype(np.float32))
+    if step % 170 == 0:
+      print(
+        "Training loss (for one batch) at step %d: %.4f \r"
+        % (step, float(loss_value))
+      )
+
+      print(
+        "Val loss (for one batch) at step %d: %.4f \r"
+        % (step, float(val_loss))
+      )
+      loss.append(loss_value)
+      vloss.append(val_loss)
+      print("Seen so far: %s samples \r" % ((step + 1) * 1))
+  diff = prev_loss - val_loss
+  prev_loss = val_loss
+  if abs(diff) < 1e-4:
+    print('\n **************************** Breaking training *************************\n')
+    print(
+      "Training loss (for one batch) at step %d: %.4f \r"
+      % (step, float(loss_value))
+    )
+    loss.append(loss_value)
+    print("Seen so far: %s samples \r" % ((step + 1) * 1))
+    train_acc = train_acc_metric.result()
+    print("Training acc over epoch: %.4f" % (float(train_acc),))
+    acc.append(float(train_acc))
+    break
+
+  train_acc = train_acc_metric.result()
+  print("Training acc over epoch: %.4f" % (float(train_acc),))
+  acc.append(float(train_acc))
+  print("Time taken: %.2fs" % (time.time() - start_time))
+  train_acc_metric.reset_states()
+
+
+with open('metrics/RNN_hw2/acc.txt', 'w') as f:
+  for i in acc:
+    f.write(str(i) + '\n')
+
+with open('metrics/RNN_hw2/loss.txt', 'w') as f:
+  for i in loss:
+    f.write(str(i.numpy()) + '\n')
+
+with open('metrics/RNN_hw2/val_loss.txt', 'w') as f:
+  for i in vloss:
+    f.write(str(i.numpy()) + '\n')
+
+model.save('models/RNN_hw2/model.h5')
+
+acc = pd.read_csv('metrics/RNN_hw2/acc.txt', header=None).values
+loss = pd.read_csv('metrics/RNN_hw2/loss.txt', header=None).values
+val_loss = pd.read_csv('metrics/RNN_hw2/val_loss.txt', header=None).values
+
+loss_c = loss.copy()
+loss = [np.average(loss[i : i+ 2]) for i in range(0,len(loss_c)-2,2)]
+
+vloss = [np.average(val_loss[i : i+ 2]) for i in range(0,len(val_loss)-2,2)]
+
+
+# Plots
+import matplotlib.pyplot as plt
+
+#plot accuracy
+plt.plot(acc)
+plt.title('Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.savefig('metrics/RNN_hw2/acc.png')
+plt.show()
+
+#plot loss
+plt.plot(loss, alpha = 0.5)
+plt.plot(vloss, c = 'orange', alpha = 0.5)
+plt.title('Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['train loss', 'val loss'])
+plt.savefig('metrics/RNN_hw2/loss.png')
+plt.show()
---
+++ Assignment 4/q1_test2.py
@@ -0,0 +1,170 @@
+import os
+from pprint import pprint
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+from data_loader import load_hw, data_loader_cv
+from tensorflow import keras
+import time
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+###############     for hw dataset
+
+X_train, y_train, X_val, y_val, X_test, y_test = load_hw()
+
+############### Load Rnn
+
+model = tf.keras.models.load_model('models/RNN_hw2/model.h5')
+
+
+####################### train
+pred = []
+for i in X_train:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_train = np.argmax(y_train, axis = 2)
+
+cf = classification_report(y_train, pred)
+print(cf)
+with open('metrics/RNN_hw2/train_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_train, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/RNN_hw2/train_confusion_matrix.png')
+plt.show()
+
+################## val
+pred = []
+for i in X_val:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_val = np.argmax(y_val, axis = 2)
+
+cf = classification_report(y_val, pred)
+print(cf)
+with open('metrics/RNN_hw2/val_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_val, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/RNN_hw2/val_confusion_matrix.png')
+plt.show()
+
+
+################## test
+pred = []
+for i in X_test:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_test = np.argmax(y_test, axis = 2)
+
+cf = classification_report(y_test, pred)
+print(cf)
+with open('metrics/RNN_hw2/test_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_test, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/RNN_hw2/test_confusion_matrix.png')
+plt.show()
+############################ LSTM
+
+############### Load LSTM
+
+model = tf.keras.models.load_model('models/LSTM_hw2/model.h5')
+
+
+####################### train
+pred = []
+for i in X_train:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_train = np.argmax(y_train, axis = 2)
+
+cf = classification_report(y_train, pred)
+print(cf)
+with open('metrics/LSTM_hw2/train_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_train, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/LSTM_hw2/train_confusion_matrix.png')
+plt.show()
+
+################## val
+pred = []
+for i in X_val:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_val = np.argmax(y_val, axis = 2)
+
+cf = classification_report(y_val, pred)
+print(cf)
+with open('metrics/LSTM_hw2/val_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_val, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/LSTM_hw2/val_confusion_matrix.png')
+plt.show()
+
+
+
+################## test
+pred = []
+for i in X_test:
+  pred.append(np.argmax(model.predict(i.astype(np.float32))))
+
+y_test = np.argmax(y_test, axis = 2)
+
+cf = classification_report(y_test, pred)
+print(cf)
+with open('metrics/LSTM_hw2/test_report.txt', 'w') as f:
+  f.write(cf) 
+
+cm = confusion_matrix(y_test, pred)
+ax= plt.subplot()
+sns.heatmap(cm, annot=True, fmt='g', ax=ax) #annot=True to annotate cells, ftm='g' to disable scientific notation
+
+# labels, title and ticks
+ax.set_xlabel('Predicted labels');ax.set_ylabel('True labels')
+ax.set_title('Confusion Matrix')
+ax.xaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA']); ax.yaxis.set_ticklabels(['a', 'ai', 'bA', 'chA', 'dA'])
+plt.savefig('metrics/LSTM_hw2/test_confusion_matrix.png')
+plt.show()
