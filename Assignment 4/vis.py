d3a50a0c528e43ad3422abe9a6de5089b1be2e1d
Prajwal Sood <prajwalsood@gmail.com>
2022-05-25 19:01:10 +0530

    Course still hurt me :(

--- Assignment 4/vis.py
+++ Assignment 4/vis.py
@@ -8,7 +8,10 @@ import matplotlib.pyplot as plt
 X_train, y_train, X_val, y_val, X_test, y_test = load_hw()
 
 
-np.argmax(y_train[:10], axis = 2)
+for i in(np.argmax(y_train[:25], axis = 2):
+  if i == 0:
+    print(i)
+
 plt.plot(X_train[8][0, :, 0], X_train[8][0, :, 1], 'xb-')
 plt.title('a')
 plt.show()
